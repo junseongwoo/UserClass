a5a3d031e5a4c2bb5cc4b548b400907abd082c4e
junseongwoo <wnstjd4852@gmail.com>
2023-09-05 23:42:56 +0900

    Image Control Class 구현 중

---
+++ Class/ImageHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserClass
+{
+    class ImageHelper
+    {
+        // 
+
+        /// <summary>
+        /// LockBit 하지 않고 Bitmap Load 
+        /// </summary>
+        /// <param name="filePath">파일 경로</param>
+        /// <returns></returns>
+        private Bitmap LoadBitmapUnlocked(string filePath)
+        {
+            using (Bitmap bitmap = new Bitmap(filePath))
+            {
+                return new Bitmap(bitmap);
+            }
+        }
+
+        /// <summary>
+        /// 이미지 저장하기 
+        /// </summary>
+        /// <param name="image">이미지</param>
+        /// <param name="filePath">파일 경로</param>
+        private void SaveImage(Image image, string filePath)
+        {
+            string fileExtension = Path.GetExtension(filePath);
+
+            switch (fileExtension.ToLower())
+            {
+                case ".bmp":
+                    image.Save(filePath, ImageFormat.Bmp);
+                    break;
+                case ".jpeg":
+                case ".jpg":
+                    image.Save(filePath, ImageFormat.Jpeg);
+                    break;
+                case ".png":
+                    image.Save(filePath, ImageFormat.Png);
+                    break;
+                default:
+                    throw new NotSupportedException("Unkown fie Extension" + fileExtension);
+            }
+        }
+
+
+
+
+
+    }
+}
